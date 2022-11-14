a541215199b8a0461466681f3f9a1c4aa2b7bc43
MrMendez <hirammendez000@gmail.com>
2022-11-14 09:38:31 -0600

    entity and DTOs added

---
+++ src/main/java/com/escuelita/demo/controllers/dtos/responses/CreateMoveResponse.java
@@ -0,0 +1,11 @@
+package com.escuelita.demo.controllers.dtos.responses;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class CreateMoveResponse {
+    private Long id;
+
+    private String name;
+}
