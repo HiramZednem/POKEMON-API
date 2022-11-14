87a1984a8365c796c7cf7c904eed982adc7ebda7
MrMendez <hirammendez000@gmail.com>
2022-11-14 10:57:25 -0600

    Entity and DTOs added

---
+++ src/main/java/com/escuelita/demo/entities/Type.java
@@ -0,0 +1,19 @@
+package com.escuelita.demo.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+public class Type {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String type;
+}
